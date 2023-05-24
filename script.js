85f67581fded0a01e068ff1aa46b19b4aa8c6972
MFarid196 <133039496+MFarid196@users.noreply.github.com>
2023-05-25 01:24:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function hitungluas () {
+    var sisiluas = document.getElementById("sisi-luas").value;
+
+    document.getElementById("output-luas").innerHTML = '<span>'+sisiluas+;
+    console.log(sisiluas);
+}
